3daa89b523b9f6218499bd9ac26597a57f88824b
Petar Björkman <bagar@Petars-MacBook-Pro.local>
2019-11-20 14:44:17 +0100

    Inloggning under construction

---
+++ DBConnection.cs
@@ -0,0 +1,18 @@
+namespace KITWTF1.Pages
+{
+    public class DBConnection
+    {
+        public int id { get; set; }
+
+        public string userName { get; set; }
+
+        public string passWord { get; set; }
+
+        public string lastlogin { get; set; }
+
+        public string lastCommunication { get; set; }
+
+        public string relative{ get; set; }
+
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,14 +4,50 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Dapper;
+using System.Data.SqlClient;
+
 
 namespace KITWTF1.Pages
 {
     public class IndexModel : PageModel
     {
+        public string email {get; set;}
+        
+        public string password {get;set;}
+        public int id {get; set;}
+      
+         public  List<DBConnection> users = new List<DBConnection>();
         public void OnGet()
         {
+                
+     
+        }
 
+     int GetUsers(string email,string password)
+        {
+                 string connectionString="server=40.85.84.155;Database=skk8;User Id=student8;Password=YH-student@2019";
+                 using (SqlConnection connection = new SqlConnection(connectionString))
+                 {       
+                    
+                                     var query =   connection.Query<DBConnection>($"select id,userName,passWord from dbo.[users] where userName='{email}'and passWord ='{password}'");
+                                     users= new List<DBConnection>(query);  
+                                     
+                                     foreach (var item in query)
+                                     {
+                                           id = item.id;
+                                          
+                                     }
+                                            Console.WriteLine("id" +email+password+id);
+                                      return id;
+                 }
+
+
+        }
+         public void OnPost()
+        {
+               
+            GetUsers(email,password);
         }
     }
 }
